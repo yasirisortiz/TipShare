03d1a178361d6520843642ae2e963bf1975d38d7
Yasiris Ortiz <yasirisortiz24@gmail.com>
2020-11-30 16:32:42 -0500

    finish with the main features of the calculator

--- src/main/java/yasirisortiz/com/tipshare/MainActivity.java
+++ src/main/java/yasirisortiz/com/tipshare/MainActivity.java
@@ -3,12 +3,41 @@ package yasirisortiz.com.tipshare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+   public Button button;
+   private Toolbar toolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.calculate_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public boolean onCreateOptionMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+
     }
 }
